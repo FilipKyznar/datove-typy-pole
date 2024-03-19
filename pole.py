bf946e11a416a964239c6d8979cecd553a73b091
Filip.Kyznar <kyznarf@gmail.com>
2024-03-19 13:35:11 +0100

    pole

---
+++ pole.py
@@ -0,0 +1,24 @@
+cislo = int(1)
+cislo_float = float(3.23)
+text = str ("bfgdh")
+Boolean = True
+
+arrays = ["Ford", "Porsche", "Audi", "BMW", "Mercedes", "Škoda" ]
+
+print (arrays)
+
+print (arrays[1])
+
+for x in arrays:
+    print (x)
+
+
+
+
+arrays = ["Ford", "Porsche", "Audi", "BMW", "Mercedes", "Škoda" ]
+
+print(arrays[0])
+
+for x in range(6):
+    print(x+1, arrays[x])
+    
